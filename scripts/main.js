49687daecaf2ef4a755d18353f5c4e56b1088696
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-22 18:22:14 +0200

    added click on body to hide the menu toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 //DOC ready
 $(document).ready(function() {
   //Jag har provat en massa funktioner som jag lämnar kvar för att komma ihåg i framtiden
-
+  $('body').click(function(){
+    $('.navHeaderCollapse').collapse('hide');
+  })
 
   // var visible = false;
   // var onPageViewName = $('#onPage-view-name').contents();
