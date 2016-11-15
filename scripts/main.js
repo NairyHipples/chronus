31167611716294b894ae7fffb628635087ac1fff
Jonatan Östling <jonatan.ostling@gmail.com>
2016-11-15 01:52:55 +0100

    Fix test

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,5 @@ $(document).ready(function() {
   $('body').click(function(){
     $('.navHeaderCollapse').collapse('hide');
   });
+  $("#element").css("height", 0.9*$(window).height());
 }); // end DOC ready
